6cea15ac2a628b1aabb913a66356d79cba293b80
Shubhanshu-Fartyal <156800803+Shubhanshu-Fartyal@users.noreply.github.com>
2026-07-31 19:16:03 +0530

    updated file

--- SQL/04_data_cleaning.sql
+++ SQL/04_data_cleaning.sql
@@ -10,14 +10,3 @@ USING unit_cost_usd::NUMERIC;
 ALTER TABLE Products
 ALTER COLUMN unit_price_usd TYPE NUMERIC(10,2)
 USING unit_price_usd::NUMERIC;
-
-
-
-SELECT DISTINCT birthday
-FROM Customers;
-
-SELECT DISTINCT open_date
-FROM Stores;
-
-SELECT DISTINCT order_date
-FROM Sales;
