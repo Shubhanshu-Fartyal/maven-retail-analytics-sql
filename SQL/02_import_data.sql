c066d52fa054a760a5ed87c461a2f21e90c118c7
Shubhanshu-Fartyal <156800803+Shubhanshu-Fartyal@users.noreply.github.com>
2026-08-01 18:37:03 +0530

    added personal comments
    Updated data types for various columns in multiple tables to accommodate changes in data format and length requirements.

--- SQL/02_import_data.sql
+++ SQL/02_import_data.sql
@@ -23,8 +23,10 @@ ALTER COLUMN delivery_date TYPE TEXT;
 
 ALTER TABLE Products ALTER COLUMN unit_cost_usd TYPE TEXT;
 ALTER TABLE Products ALTER COLUMN unit_price_usd TYPE TEXT;
+--because the data had '$' sign but I had mistakenly assigned NUMERIC datatype to them
 
 
 
 ALTER TABLE Customers 
 ALTER COLUMN state_code TYPE VARCHAR(100);
+--I underestimated the length of state_code
