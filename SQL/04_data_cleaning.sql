3fbddec6c6b36d818b98133220c942769fd1c919
Shubhanshu-Fartyal <156800803+Shubhanshu-Fartyal@users.noreply.github.com>
2026-08-01 21:10:01 +0530

    added queries
    Added data cleaning steps for date formatting and foreign key constraint.

--- SQL/04_data_cleaning.sql
+++ SQL/04_data_cleaning.sql
@@ -3,6 +3,8 @@ SET
     unit_cost_usd = TRIM(REPLACE(unit_cost_usd, ',', '')),
     unit_price_usd = TRIM(REPLACE(unit_price_usd, ',', ''));
 
+
+
 ALTER TABLE Products
 ALTER COLUMN unit_cost_usd TYPE NUMERIC(10,2)
 USING unit_cost_usd::NUMERIC;
@@ -10,3 +12,35 @@ USING unit_cost_usd::NUMERIC;
 ALTER TABLE Products
 ALTER COLUMN unit_price_usd TYPE NUMERIC(10,2)
 USING unit_price_usd::NUMERIC;
+
+
+
+ALTER TABLE Customers
+ALTER COLUMN birthday TYPE DATE
+USING TO_DATE(birthday, 'MM/DD/YYYY');
+
+ALTER TABLE Stores
+ALTER COLUMN open_date TYPE DATE
+USING TO_DATE(open_date, 'MM/DD/YYYY');
+
+ALTER TABLE Exchange_Rates
+ALTER COLUMN date TYPE DATE
+USING TO_DATE(date, 'MM/DD/YYYY');
+
+ALTER TABLE Sales
+ALTER COLUMN order_date TYPE DATE
+USING TO_DATE(order_date, 'MM/DD/YYYY');
+
+ALTER TABLE sales
+ALTER COLUMN delivery_date TYPE DATE
+USING CASE
+    WHEN delivery_date IS NULL OR TRIM(delivery_date) = '' THEN NULL
+    ELSE TO_DATE(delivery_date, 'MM/DD/YYYY')
+END;
+
+
+
+ALTER TABLE Sales
+ADD CONSTRAINT fk_sales_exchange_rate
+FOREIGN KEY (order_date, currency_code)
+REFERENCES Exchange_Rates(date, currency);
